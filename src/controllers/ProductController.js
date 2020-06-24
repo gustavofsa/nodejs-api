18a1f4f82232ebfc034b07ba6658e8d102d28901
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-23 23:31:12 -0300

    CRUD completed

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -13,5 +13,23 @@ module.exports = {
     const product = await Product.create(req.body);
 
     return res.json(product);
+  },
+
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+
+    return res.json(product);
+  },
+
+  async updade(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+
+    return res.send();
   }
 };
